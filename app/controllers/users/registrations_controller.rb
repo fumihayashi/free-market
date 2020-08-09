39d84b783f984485d4e66e2a36a7190f13362b02
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-09 14:59:53 +0900

    after_sign_in_path_for

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def completed
   end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -74,4 +74,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 end
