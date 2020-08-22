34f7ae48c2eb7ddab478e43a311845e0f3c913ed
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-22 19:39:19 +0900

    assosiation adressmodel and prefecturemodel

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,7 @@ class Address < ApplicationRecord
     belongs_to :user, optional: true
 
     validates :city, :house_number, :postal_code, presence: true
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :prefecture
 end
