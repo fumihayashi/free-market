8361e5315f8b57839a5f1bdc11cbcee7ee330cec
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-26 21:01:16 +0900

    validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-
+  validates :images, length: { minimum: 1, maximum: 5, message: "の数が不正です" } 
+  
   belongs_to :category
 
   extend ActiveHash::Associations::ActiveRecordExtensions
