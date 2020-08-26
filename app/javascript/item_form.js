53d35ff2433734fd9f7b358ef319267a3aff501e
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-27 03:26:34 +0900

    fix

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,4 +1,5 @@
 document.addEventListener('turbolinks:load', function () {
+  
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
 
   function newFileField(index) { //新規画像投稿用のfile_fieldを作成しappendする。
@@ -21,14 +22,11 @@ document.addEventListener('turbolinks:load', function () {
   /////////////////////////////////////////////
   /////////file_fieldが変化した時/////////
   ////////////////////////////////////////////
+  
   $("#image-file-fields").on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
-    console.table(e.target.files);
-    console.log("画像が選択されました")
     const file = e.target.files[0];
     let index = $(this).data("index");
-    console.log("選択した画像のindex=", index);
     const blob_url = window.URL.createObjectURL(file); //選択された画像をblob url形式に変換する。
-    console.log(blob_url);
     const preview_html = `<img src="${blob_url}" width="20%">`;
     $("#select-image-button").before(preview_html);
     index += 1;
