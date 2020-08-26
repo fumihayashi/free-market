d332e2a6692f4e913ec8aab8fdc54f9e5d619be6
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-27 05:49:57 +0900

    imprement_editbutton

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function () {
-  
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
+  
   function buildImagePreview(blob_url, index) { //選択した画像ファイルのプレビューを表示する。
     html = `
             <div class="item-image new" data-index=${index}>
@@ -39,10 +39,23 @@ document.addEventListener('turbolinks:load', function () {
   /////////file_fieldが変化した時/////////
   ////////////////////////////////////////////
   
-  $("#image-file-fields").on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+  $("#image-file-fields").on("change", `input[type="file"]`, function (e) {
+    //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
     const file = e.target.files[0];
     let index = $(this).data("index");
+    if (!file) {
+      console.log("何も選択しませんでした");
+      const delete_button = $(`.item-image[data-index="${index}"]`).find(".item-image__buttons--delete");
+      delete_button.trigger("click");
+      return false;
+    }
     const blob_url = window.URL.createObjectURL(file); //選択された画像をblob url形式に変換する。
+    if ($(`.item-image[data-index="${index}"]`)[0]) {
+      console.log("画像の変更を行います");
+      const preview_image = $(`.item-image[data-index="${index}"]`).children("img");
+      preview_image.attr("src", blob_url);
+      return false;
+    }
     const preview_html = buildImagePreview(blob_url, index);
     $("#select-image-button").before(preview_html);
     index += 1;
@@ -56,11 +69,19 @@ document.addEventListener('turbolinks:load', function () {
   ////////////////////////////////////////////
   $("#selected-item-images").on("click", ".item-image__buttons--delete", function (e) {
     const index = $(this).parents(".item-image").data("index");
-    console.log(index, "番目の画像を削除します")
     $(this).parents(".item-image").remove();
     $(`#item_images_attributes_${index}__destroy`).prop("checked", true);
     $(`#item_images_attributes_${index}_src`).remove();
   });
   /////////画像の削除ボタンをクリックした時ここまで/////////
+
+  /////////////////////////////////////////////
+  /////////画像の編集ボタンをクリックした時/////////
+  ////////////////////////////////////////////
+  $("#selected-item-images").on("click", ".item-image__buttons--edit", function (e) {
+    const index = $(this).parents(".item-image").data("index");
+    $(`#item_images_attributes_${index}_src`).trigger("click");
+  });
+  /////////画像の編集ボタンをクリックした時ここまで/////////
 
 });
