ca391fbf2c96f5dcb1f8513e3e887e093dbcb643
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-27 03:05:49 +0900

    filefield_start(#select-image-button)

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,3 +1,27 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
+
+  ///////////////////////////////////////////////////////////////
+  /////////画像の投稿ボタン（グレーのブロック）をクリックした時。/////////
+  ///////////////////////////////////////////////////////////////
+  $("#select-image-button").on("click", function () {
+    const file_field = $("#item_images_attributes_0_src"); // 新規画像投稿用のfile_fieldを取得する。
+    file_field.trigger("click"); // file_fieldをクリックさせる。
+  });
+  /////////画像の投稿ボタン（グレーのブロック）をクリックした時ここまで/////////
+
+  /////////////////////////////////////////////
+  /////////file_fieldが変化した時/////////
+  ////////////////////////////////////////////
+  $("#image-file-fields").on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+    console.table(e.target.files);
+    console.log("画像が選択されました")
+    const file = e.target.files[0];
+    const blob_url = window.URL.createObjectURL(file); //選択された画像をblob url形式に変換する。
+    console.log(blob_url);
+    const preview_html = `<img src="${blob_url}" width="20%">`;
+    $("#select-image-button").before(preview_html);
+  });
+  /////////file_fieldが変化した時ここまで/////////
+
 });
