73df15c50d01dd5ea8410773089c88d301f09314
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-30 22:38:04 +0900

    imprement_destroy

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
-  before_action :redirect_registered_user, except: [:index]
+  before_action :redirect_registered_user, except: [:index, :destroy]
 
   def index
     @card = Card.get_card(current_user.card.customer_token) if current_user.card
@@ -21,6 +21,17 @@ class CardsController < ApplicationController
     else
       redirect_to new_card_path, alert: "カードの登録に失敗しました。"
     end
+  end
+
+  def destroy
+    card = current_user.card
+
+    if card.destroy
+      redirect_to cards_path, notice: "カードの削除が完了しました。"
+    else
+      redirect_to cards_path, notice: "カードの削除に失敗しました。"
+    end
+
   end
 
   private
