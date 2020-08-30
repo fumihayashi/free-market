6f221fe99bc253d2efc1e6f3336b3fa68aaa92b9
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-30 20:40:45 +0900

    create_card_object

--- app/javascript/regist_card.js
+++ app/javascript/regist_card.js
@@ -1,4 +1,17 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('#card_form')[0]) return false; //カード登録ページではないなら以降実行しない。
-  console.log("hoge");
+  
+  Payjp.setPublicKey("pk_test_hogehogefugafuga");//公開鍵を読み込む。
+  const regist_button = $("#regist_card"); //カード入力フォームの登録ボタン。
+
+  regist_button.on("click", function (e) { //登録ボタンを押したとき（ここはsubmitではなくclickにしておく）。
+    e.preventDefault();
+      const card = {
+        number: $("#card_number_form").val(),
+        cvc: $("#cvc_form").val(),
+        exp_month: $("#exp_month_form").val(),
+        exp_year: $("#exp_year_form").val()
+      };
+      console.log(card);    
+  }); //登録ボタンを押したときここまで。
 });
