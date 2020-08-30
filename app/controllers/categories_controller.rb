a35dacaa34c52b0e4af1b5926f62c818e31b2c77
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-30 17:06:04 +0900

    dispray_product

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @items = Item.search_by_categories(@category.subtree_ids)
   end
 
 end
