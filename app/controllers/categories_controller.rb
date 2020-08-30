71e75d0316ace951a383f277f87dc5c9d4bf5ea0
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-08-30 16:51:34 +0900

    detailed_page(everycategory)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
   end
+
+  def show
+  end
+  
 end
